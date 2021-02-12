29b97f781c56de333e6c62c453fbcbb1cfec8ca2
domano <angor.mail+github@googlemail.com>
2021-02-12 18:45:20 +0100

    Simplify hero section

--- app/components/Hero.tsx
+++ app/components/Hero.tsx
@@ -1,15 +1,14 @@
 import {Transition} from "@headlessui/react";
 import Image from "next/image";
+import {useEffect, useState} from "react";
 
 
 export default function Hero() {
 
-    return <Transition
-        show={true}
-    >
-        <div className="flex flex-col lg:flex-row justify-around text-center">
-            <h1 className="my-auto text-4xl tracking-tight font-extrabold text-gray-900 sm:text-5xl md:text-6xl">
-                <Transition.Child
+    return  <div className="flex flex-col lg:flex-row justify-around text-center">
+        <h1 className="my-auto text-4xl tracking-tight font-extrabold text-gray-900 sm:text-5xl md:text-6xl">
+                <Transition
+                    show
                     // @ts-ignore
                     appear="true"
                     enter="transition duration-500 transform"
@@ -18,9 +17,10 @@ export default function Hero() {
                     leaveFrom="opacity-100"
                     leaveTo="opacity-0"
                     className="block">
-                    <span >Dino Omanovic</span>
-                </Transition.Child>
-                <Transition.Child
+                    <span>Dino Omanovic</span>
+                </Transition>
+                <Transition
+                    show
                     // @ts-ignore
                     appear="true"
                     enter="transition duration-1000 delay-500 transform"
@@ -30,19 +30,12 @@ export default function Hero() {
                     leaveTo="opacity-0"
                     className="block text-purple-700 ">
                     <span>tech consulting</span>
-                </Transition.Child>
+                </Transition>
             </h1>
-            <Transition.Child
-                // @ts-ignore
-                appear="true"
-                enter="transition duration-1000 transform"
-                enterFrom="opacity-0 rotate-180 translate-x-72"
-                leave="transition-opacity duration-150"
-                leaveFrom="opacity-100"
-                leaveTo="opacity-0"
-                className="h-96 w-96 relative mx-auto my-10">
-                    <Image src="/me.jpg" layout="fill" objectFit="cover" className="rounded-full"/>
-            </Transition.Child>
+            <div className="h-56 w-56 md:h-96 md:w-96  relative mx-auto my-10">
+            <Image src="/me.jpg" layout="fill" priority objectFit="cover" className="rounded-full "/>
+            </div>
+
+
         </div>
-    </Transition>
 }
