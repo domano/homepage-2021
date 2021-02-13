2c9402526f8d3f55224a07122a1787882bd9a1bb
domano <angor.mail+github@googlemail.com>
2021-02-14 00:37:45 +0100

    Link social profiles

--- app/components/Contact.tsx
+++ app/components/Contact.tsx
@@ -1,5 +1,8 @@
+// @ts-ignore
 import GithubSVG from "../public/social/github.svg"
+// @ts-ignore
 import XingSVG from "../public/social/xing.svg"
+// @ts-ignore
 import LinkedInSVG from "../public/social/linkedin.svg"
 
 export default function Contact() {
@@ -8,9 +11,9 @@ export default function Contact() {
             <div className="pt-3 pb-3">
                 <div className="flex flex-shrink h-8 justify-evenly ">
                 <p className="text-2xl font-extrabold tracking-tight sm:text-4xl text-purple-700">kontakt</p>
-                    <GithubSVG className="transition duration-200 ease-in-out transform hover:-translate-y-1 hover:scale-110 "/>
-                    <XingSVG className="transition duration-200 ease-in-out transform hover:-translate-y-1 hover:scale-110 "/>
-                    <LinkedInSVG className="transition duration-200 ease-in-out transform hover:-translate-y-1 hover:scale-110 "/>
+                    <a href="https://github.com/domano" ><GithubSVG  className="transition w-9 duration-200 ease-in-out transform hover:-translate-y-1 hover:scale-110 "/></a>
+                    <a href="https://www.xing.com/profile/Dino_Omanovic2/" ><XingSVG className="transition w-9 duration-200 ease-in-out transform hover:-translate-y-1 hover:scale-110 "/></a>
+                    <a href="https://www.linkedin.com/in/dino-omanovic/" ><LinkedInSVG className="transition w-9 duration-200 ease-in-out transform hover:-translate-y-1 hover:scale-110 "/></a>
                 </div>
             </div>
         </div>
