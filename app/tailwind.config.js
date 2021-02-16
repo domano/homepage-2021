255860ed5fc2a7b4858670d60cfa07b5e65025e2
domano <angor.mail+github@googlemail.com>
2021-02-16 22:52:32 +0100

    Include /lib in PurgeCSS config

--- app/tailwind.config.js
+++ app/tailwind.config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  purge: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
+  purge: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}', './lib/**/*.{js,ts,jsx,tsx}'],
   darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {
@@ -16,6 +16,10 @@ module.exports = {
           '0%,30%,100%': { transform: 'scale(1.00)' },
           '20%': { transform: 'scale(1.03)' },
         },
+        shrink: {
+          '0%,30%,100%': { transform: 'scale(1.00)' },
+          '20%': { transform: 'scale(0.97)' },
+        },
         flicker: {
           '0%, 100%': { opacity:'0' },
           '50%': { opacity: '100' },
@@ -29,6 +33,7 @@ module.exports = {
         wiggle: 'wiggle 1s ease-in-out infinite',
         swell: 'swell 1s ease-in-out infinite',
         grow: 'grow 4s ease-in-out',
+        shrink: 'shrink 4s ease-in-out',
         flicker: 'flicker 1s linear infinite',
         appear: 'appear 500ms ease-in-out ',
       }
